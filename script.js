90e7412f3486fd49dd04feef80062d88e42bdaaa
Kush Agarwal <83411262+Kush4077@users.noreply.github.com>
2021-06-28 21:11:38 +0530

    Reduced the area of on click action
    Co-Authored-By: Sumesh Mohanty <80830020+SumeMonty@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let taskbar = document.getElementsByClassName("taskbar")[0]
+let icon = document.getElementsByClassName("icon")[0]
 let start = document.getElementsByClassName("start")[0]
 
-taskbar.addEventListener("click", ()=>{
+icon.addEventListener("click", ()=>{
     if(start.style.bottom == "50px"){
         start.style.bottom = "-655px"
     }
