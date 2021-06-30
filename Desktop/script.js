2c63ab60df554202c1dfe5975cf45eb4d2382e40
Kush Agarwal <83411262+Kush4077@users.noreply.github.com>
2021-06-30 15:24:21 +0530

    Changed Taskbar Icons
    Co-Authored-By: Sumesh Mohanty <80830020+SumeMonty@users.noreply.github.com>

--- Desktop/script.js
+++ Desktop/script.js
@@ -1,7 +1,7 @@
-let icon = document.getElementsByClassName("icon")[0]
+let start_icon = document.getElementsByClassName("start_icon")[0]
 let start = document.getElementsByClassName("start")[0]
 
-icon.addEventListener("click", ()=>{
+start_icon.addEventListener("click", ()=>{
     if(start.style.bottom == "50px"){
         start.style.bottom = "-655px"
     }
