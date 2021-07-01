683df75f162b3cbc64ee552991cb0951bf8f5e79
Sumesh Mohanty <80830020+SumeMonty@users.noreply.github.com>
2021-07-01 18:08:10 +0530

    Added Frames For Application Windows
    Co-Authored-By: Kush Agarwal <83411262+Kush4077@users.noreply.github.com>

--- Desktop/script.js
+++ Desktop/script.js
@@ -1,11 +1,27 @@
+// ************************StartMenu JS************************
 let start_icon = document.getElementsByClassName("start_icon")[0]
 let start = document.getElementsByClassName("start")[0]
 
-start_icon.addEventListener("click", ()=>{
-    if(start.style.bottom == "50px"){
+start_icon.addEventListener("click", () => {
+    if (start.style.bottom == "50px") {
         start.style.bottom = "-655px"
     }
-    else{
+    else {
         start.style.bottom = "50px"
     }
 })
+// ************************StartMenu JS************************
+
+// *************************TOOLTIP JS*************************
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+// *************************TOOLTIP JS*************************
+
+// **********************RESIZABLE IFRAME JS**********************
+$(".resizable").draggable();
+$(".resizable").resizable({
+    handles: "n,s,w,e"
+});
+// **********************RESIZABLE IFRAME JS**********************
