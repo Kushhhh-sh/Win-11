42f74b22af4a8e262c744238b6c31b02cb57342b
Kush Agarwal <83411262+Kush4077@users.noreply.github.com>
2021-07-02 15:54:56 +0530

    Added JS for applications
    Co-Authored-By: Sumesh Mohanty <80830020+SumeMonty@users.noreply.github.com>

--- Desktop/script.js
+++ Desktop/script.js
@@ -25,3 +25,103 @@ $(".resizable").resizable({
     handles: "n,s,w,e"
 });
 // **********************RESIZABLE IFRAME JS**********************
+
+//********************IFRAME VISIBILITY JS********************
+function openedge(){
+    var edgeframe = document.getElementById("edgeframe")
+    var tbedge = document.getElementById("tb-edge")
+
+    if(edgeframe.style.display == "none"){
+        edgeframe.style.display = "inline"
+        tbedge.style.display = "inline"
+    }
+    else{
+        edgeframe.style.display = "none"
+        tbedge.style.display = "none"
+    }
+}
+
+function opennote(){
+    var noteframe = document.getElementById("noteframe")
+    var tbnote = document.getElementById("tb-note")
+
+    if(noteframe.style.display == "none"){
+        noteframe.style.display = "inline"
+        tbnote.style.display = "inline"
+    }
+    else{
+        noteframe.style.display = "none"
+        tbnote.style.display = "none"
+    }
+}
+
+function openpaint(){
+    var paintframe = document.getElementById("paintframe")
+    var tbpaint = document.getElementById("tb-paint")
+
+    if(paintframe.style.display == "none"){
+        paintframe.style.display = "inline"
+        tbpaint.style.display = "inline"
+    }
+    else{
+        paintframe.style.display = "none"
+        tbpaint.style.display = "none"
+    }
+}
+
+function openword(){
+    var wordframe = document.getElementById("wordframe")
+    var tbword = document.getElementById("tb-word")
+
+    if(wordframe.style.display == "none"){
+        wordframe.style.display = "inline"
+        tbword.style.display = "inline"
+    }
+    else{
+        wordframe.style.display = "none"
+        tbword.style.display = "none"
+    }
+}
+
+function openexcel(){
+    var excelframe = document.getElementById("excelframe")
+    var tbexcel = document.getElementById("tb-excel")
+
+    if(excelframe.style.display == "none"){
+        excelframe.style.display = "inline"
+        tbexcel.style.display = "inline"
+    }
+    else{
+        excelframe.style.display = "none"
+        tbexcel.style.display = "none"
+    }
+}
+
+function openppt(){
+    var pptframe = document.getElementById("pptframe")
+    var tbppt = document.getElementById("tb-ppt")
+
+    if(pptframe.style.display == "none"){
+        pptframe.style.display = "inline"
+        tbppt.style.display = "inline"
+    }
+    else{
+        pptframe.style.display = "none"
+        tbppt.style.display = "none"
+    }
+}
+
+function opencalci(){
+    var calciframe = document.getElementById("calciframe")
+    var tbcalci = document.getElementById("tb-calci")
+
+    if(calciframe.style.display == "none"){
+        calciframe.style.display = "inline"
+        tbcalci.style.display = "inline"
+    }
+    else{
+        calciframe.style.display = "none"
+        tbcalci.style.display = "none"
+    }
+}
+//********************IFRAME VISIBILITY JS********************
